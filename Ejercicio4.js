6730130db94defb73022f6765949c0395576de91
Serranomanuel <Serranomanuel106@gmail.com>
2025-10-04 19:38:46 -0500

    se aproximan los valores con math floor

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -4,9 +4,9 @@ const nota3 = 2.9;
 const nota4 = 4.0;
 
 // Cálculos
-let suma = nota1 + nota2 + nota3 + nota4; // suma total de las cuatro notas
-let promedio = suma / 4; // promedio aritmético
-let resultado = (nota1 + nota2) * (nota3 + nota4) / 2; // expresión solicitada
+let suma = Math.floor(nota1 + nota2 + nota3 + nota4); // suma total de las cuatro notas
+let promedio = Math.floor(suma / 4); // promedio aritmético
+let resultado = Math.floor((nota1 + nota2) * (nota3 + nota4) / 2); // expresión solicitada
 
 // Mostrar resultados por consola
 console.log(`Suma de las notas: ${suma}`);
